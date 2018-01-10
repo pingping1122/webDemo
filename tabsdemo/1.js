171ba4a19d987281245e9068e84b311f1171a73a
pingping1122 <2652902589@qq.com>
2018-01-10 18:04:27 +0800

    增加tabdemo

---
+++ tabsdemo/1.js
@@ -0,0 +1,10 @@
+/**
+ * Created by lpp on 2017/3/31.
+ */
+$(function(){
+    $(".tab_menu ul li").click(function(){
+        $(this).addClass("on").siblings().removeClass("on"); //切换选中的按钮高亮状态
+        var index=$(this).index(); //获取被按下按钮的索引值，需要注意index是从0开始的
+        $(".tab_box > div").eq(index).show().siblings().hide(); //在按钮选中时在下面显示相应的内容，同时隐藏不需要的框架内容
+    });
+});
